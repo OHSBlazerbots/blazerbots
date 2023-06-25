6de947678a63d6af1c2d7991f8a47a264cdbd548
aselenke <aselenke96@gmail.com>
2023-06-24 19:40:09 -0600

    added new pages and fixed css

---
+++ src/pages/history.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar/navBar";
+
+function historyPage() {
+  return (
+    <div>
+      <NavBar />
+      <header>
+        <h1> Welcome to the history page!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default historyPage;
---
+++ src/pages/sponsors.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar/navBar";
+
+function sponsorsPage() {
+  return (
+    <div>
+      <NavBar />
+      <header>
+        <h1> Welcome to the sponsors page!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default sponsorsPage;
