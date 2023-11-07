6c00f57e8ef26fdd024cbc63156fd9e5fa8c7d6a
Maria Nguyen <47044508+Pinkwinx@users.noreply.github.com>
2023-11-07 23:52:23 +0000

    Fixing nav bar by editing links

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BasePage from "../components/BasePage/BasePage";
 
 const page = () => (
-  <BasePage pageName="Home">Welcome to the home page!</BasePage>
+  <BasePage pageName="Calander">Welcome to the calander page!</BasePage>
 );
 
 export default page;
