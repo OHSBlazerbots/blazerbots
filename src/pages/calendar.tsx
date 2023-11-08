bfe886068431c970feb9f27098a46092648eae0a
jmullercuber <jmullercuber@gmail.com>
2023-11-07 19:16:37 -0700

    Fix typo typo in src/pages/calendar.tsx

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BasePage from "../components/BasePage/BasePage";
 
 const page = () => (
-  <BasePage pageName="Calender">Welcome to the calender page!</BasePage>
+  <BasePage pageName="Calendar">Welcome to the calendar page!</BasePage>
 );
 
 export default page;
