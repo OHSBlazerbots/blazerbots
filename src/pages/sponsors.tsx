6724b2e7a1ff2d4301463c5ee83419cc39bec47f
Sam Goldman <6364482+samgoldman@users.noreply.github.com>
2023-11-10 15:58:38 -0700

    Angle -> angel

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -5,14 +5,14 @@ import logo_ccsd from "../images/logo_ccsd.png";
 import logo_comcast from "../images/logo_comcast.png";
 import logo_haas from "../images/logo_haas.png";
 import logo_ccsf from "../images/logo_ccsf.png";
-import logo_anglebotics from "../images/logo_anglebotics.svg";
+import logo_angelbotics from "../images/logo_angelbotics.svg";
 import logo_ace from "../images/logo_ace.png";
 import logo_fastenal from "../images/logo_fastenal.png";
 import logo_brothers from "../images/logo_brothers.svg";
 import logo_harbor_freight from "../images/logo_harbor_freight.svg";
 
 const styles = {
-  anglebots: {
+  angelbots: {
       background: 'red'
   }
 };
@@ -22,7 +22,7 @@ const pinkSponsors = [
   {name:"Comcast", logo:logo_comcast},
   {name:"Gene Haas Foundation", logo:logo_haas},
   {name:"Cherry Creek Schools Foundation", logo:logo_ccsf},
-  {name:"Anglebotics", logo:logo_anglebotics, style: styles.anglebots},
+  {name:"Angelbotics", logo:logo_angelbotics, style: styles.angelbots},
   {name:"ACE Hardware", logo:logo_ace, description: "Buckley Square"},
   {name:"Fastenal", logo:logo_fastenal},
   {name:"Brothers", logo:logo_brothers},
