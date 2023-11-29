955bf17c9b7ac5505628715ac156b8ec8e68ccba
Joey Muller <jmullercuber@gmail.com>
2023-11-28 22:09:34 -0700

    refactor: sponsor colors to constants file

--- src/pages/how-to-be-sponsors.tsx
+++ src/pages/how-to-be-sponsors.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 import BasePage from "../components/BasePage/BasePage";
+import { SponsorTiers } from "../components/constants";
 
 const title = "Become a Sponsor"
 const sponsorPacketUrl = "https://drive.google.com/file/d/1zJ_RThOwgtSONbEUVSGEswerpjsNMbjc/preview";
@@ -11,25 +11,20 @@ const style = {
   "margin": "auto",
 }
 
-const bronze = {
-  "color": "#cd7f32",
-  "font-weight": "bold"
-};
+const makeSponsorTierText = (tier: keyof typeof SponsorTiers) => {
+  // Lookup color associated with sponsorship tier
+  const tierColor = SponsorTiers[tier].color
+  
+  const tierStyle = {
+    color: tierColor,
+    "font-weight": "bold"
+  }
 
-const silver = {
-  "color": "#c0c0c0",
-  "font-weight": "bold"
-};
+  return (
+    <span style={tierStyle}>{tier}</span>
+  )
+}
 
-const gold = {
-  "color": "#ffd700",
-  "font-weight": "bold"
-};
-
-const pink = {
-  "color": "#FF007f",
-  "font-weight": "bold"
-};
 
 const pdfContainerStyle = {
   "aspect-ratio": "8.5/11",
@@ -43,7 +38,7 @@ const page = () => (
       <p>Do you own, work for, or know a business that would like to support our program and receive advertisement opportunities in return?</p>
       <p>
         We're always seeking funding and sponsors to support our budget, which was grown to nearly $20,000 per year.
-        We offer four tiers of sponsorship: <span style={bronze}>Bronze</span>, <span style={silver}>Silver</span>, <span style={gold}>Gold</span>, and <span style={pink}>Pink</span>. Each offers logo placement with increasing visibility, starting with our website for Bronze and culminating in a large logo on our competition robot.
+        We offer four tiers of sponsorship: {makeSponsorTierText("Bronze")}, {makeSponsorTierText("Silver")}, {makeSponsorTierText("Gold")}, and {makeSponsorTierText("Pink")}. Each offers logo placement with increasing visibility, starting with our website for Bronze and culminating in a large logo on our competition robot.
         See our sponsorship packet below for details!
       </p>
       <p>Contact Sam Goldman at <a href="mailto:sgoldman@cherrycreekschools.org?subject=BlazerBots Sponsorship Inquiry">sgoldman@cherrycreekschools.org</a> with any questions or to get started.</p>
