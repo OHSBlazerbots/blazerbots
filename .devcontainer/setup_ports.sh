9e6722f52af50c6612bdf67f95dd782673b5f3ab
jmullercuber <jmullercuber@gmail.com>
2023-12-01 05:56:44 +0000

    setup ports error handling

--- .devcontainer/setup_ports.sh
+++ .devcontainer/setup_ports.sh
@@ -4,11 +4,11 @@ set -euxo pipefail
 # Early exit if not in GitHub Codespace
 if [[ "${CODESPACES-}" != "true" ]]; then
     echo "No ports to change since not in Codespace. Skipping..."
-    exit 1
+    exit 0
 fi
 
-# Set visibility to public
-gh codespace ports visibility 8000:public -c "$CODESPACE_NAME"
+# Try to set visibility to public (ignore if command fails)
+gh codespace ports visibility 8000:public -c "$CODESPACE_NAME" || true
 
 # Somewhat hacky, but another attempt to make the website port public.
 # This works by making the command to set port visibility execute when opening every shell
