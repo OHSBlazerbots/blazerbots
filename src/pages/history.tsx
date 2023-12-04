d5d524a9fb4ceb98a54a44bc7f5027afcbe5f98d
jmullercuber <jmullercuber@gmail.com>
2023-12-04 08:52:24 +0000

    add 2023 pics to history page

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import BasePage from "../components/BasePage/BasePage";
 
+import Carousel from "react-bootstrap/Carousel";
+
+import robot_2023_0 from "../images/23photos/23regionalShowRobot0.jpg";
+import robot_2023_1 from "../images/23photos/23regionalShowRobot1.jpg";
+import robot_2023_2 from "../images/23photos/23regionalShowRobot2.jpg";
+import robot_2023_3 from "../images/23photos/23regionalShowRobot3.jpg";
+
+const carouselItemStyle = {
+  width: "100%"
+}
+
+const makeCarouselItem = (image: string) => (
+  <Carousel.Item>
+    <img src={image} style={carouselItemStyle} />
+  </Carousel.Item>
+);
+
+const images2023 = [robot_2023_0, robot_2023_1, robot_2023_2, robot_2023_3];
+
 const page = () => (
-  <BasePage pageName="History">Welcome to the history page!</BasePage>
+  <BasePage pageName="History">
+    Welcome to the history page!
+    <h2>2023 - Charged Up! ⚡</h2>
+    <Carousel>{images2023.map(makeCarouselItem)}</Carousel>
+  </BasePage>
 );
 
 export default page;
