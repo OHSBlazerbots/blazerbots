b2c2b1a643391802b5c2dc86beebe9a1522a5fb9
Joey Muller <jmullercuber@gmail.com>
2024-01-03 04:53:20 +0000

    wip: improving footer style + more links

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { footer, linkActive } from './footer.module.css';
+import { footer } from './footer.module.css';
 import { AiOutlineMail } from "@react-icons/all-files/ai/AiOutlineMail"
 import { AiFillInstagram } from "@react-icons/all-files/ai/AiFillInstagram"
+import { AiFillGithub } from "@react-icons/all-files/ai/AiFillGithub";
+import { AiFillAlert } from "@react-icons/all-files/ai/AiFillAlert";
+import type { IconType } from "@react-icons/all-files";
 
 const stopClickPropagation:React.MouseEventHandler = event => event.stopPropagation();
+
+const LinkedContactInfo = (displayIcon: IconType, displayText: string, link: string) => (
+    <span>
+        {displayIcon({})}
+        &nbsp;
+        <a href={link} className="text-white" onClick={stopClickPropagation} >{displayText}</a>
+    </span>
+)
+
+const EmailInfo = LinkedContactInfo(AiOutlineMail, "ohsblazerbots@gmail.com", "mailto:ohsblazerbots@gmail.com")
+const InstagramInfo = LinkedContactInfo(AiFillInstagram, "Instagram", "https://instagram.com/ohsblazerbots")
+const GitHubInfo = LinkedContactInfo(AiFillGithub, "GitHub", "https://github.com/OHSBlazerbots")
+// TODO: replace Alert with TBA logo
+const TBAInfo = LinkedContactInfo(AiFillAlert, "TheBlueAlliance", "https://www.thebluealliance.com/team/3807")
 
 function Footer() {
   return ( 
@@ -12,39 +29,12 @@ function Footer() {
         <Container>
             <Row>
                 <Col>
-                    Contact Us!
+                    <div>Contact Us!</div>
+                    <div>{EmailInfo}</div>
                 </Col>
                 <Col>
-                    {/* This is for the center of the footer, row 1 */}
-                </Col>
-                <Col>
-                    Follow Us On Social Media!
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <AiOutlineMail />
-                    <span> </span>
-                    <a href="mailto:ohsblazerbots@gmail.com" className="text-white" onClick={stopClickPropagation} >ohsblazerbots@gmail.com</a>
-                </Col>
-                <Col>
-                    {/* This is for the center of the footer, row 2 */}
-                </Col>
-                <Col>
-                    <AiFillInstagram /> 
-                    <span> </span>
-                    <a href="https://instagram.com/ohsblazerbots" className="text-white" onClick={stopClickPropagation} >Instagram</a>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    {/* This is for the left of the footer, row 3 */}
-                </Col>
-                <Col>
-                    {/* This is for the center of the footer, row 3 */}
-                </Col>
-                <Col>
-                    {/* This is for the right of the footer, row 3 */}
+                    <div>Follow Us On Social Media!</div>
+                    <div>{InstagramInfo} | {GitHubInfo} | {TBAInfo}</div>
                 </Col>
             </Row>
         </Container>
