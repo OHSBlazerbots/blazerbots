608f2496df03d7d74359f15ab3a64de008577aa8
Sam Goldman <6364482+samgoldman@users.noreply.github.com>
2024-01-03 23:12:05 -0700

    Feat: Add History (#25)
    * Initial history implementation & content
    
    * fix warnings; remove redundant showImages param
    
    * display TheBlueAlliance link on same line as title
    
    * refactor: imports
    
    * refactor: icon css
    
    * organize history in collapsible accordion
    
    * fix carousel width based on screen size; portrait and landscape images all work
    
    * refactor: formatting; carousel dark mode
    
    ---------
    
    Co-authored-by: Joey Muller <jmullercuber@gmail.com>

---
+++ src/components/HistoryCard/historyCard.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Accordion, Carousel } from "react-bootstrap";
+
+import {
+  title as titleStyle,
+  TBAIcon as TBAIconStyle,
+  iconContainer as iconContainerStyle,
+  carouselItem as carouselItemStyle,
+} from "./historyCard.module.css";
+import icon_tba from "../../images/icon_tba.png";
+
+interface HistoryCardProps {
+  year: string;
+  name: string;
+  blurb: string;
+  images: string[];
+  showTBA?: boolean;
+}
+
+const makeCarouselItem = (image: string, index: number) => (
+  <Carousel.Item key={index}>
+    <img src={image} className={carouselItemStyle} />
+  </Carousel.Item>
+);
+
+const TBALink = ({ year }: { year: string }) => {
+  const address = "https://www.thebluealliance.com/team/3807/" + year;
+
+  return (
+    <div className={iconContainerStyle}>
+      <a href={address} target="_blank">
+        <img src={icon_tba} className={TBAIconStyle} />
+      </a>
+    </div>
+  );
+};
+
+const HistoryCard = ({
+  year,
+  name,
+  blurb,
+  images,
+  showTBA = true,
+}: HistoryCardProps) => {
+  return (
+    <Accordion.Item eventKey={year}>
+      <Accordion.Header>
+        <p className={titleStyle}>
+          {year}: {name}
+        </p>
+        {showTBA && <TBALink year={year} />}
+      </Accordion.Header>
+      <Accordion.Body>
+        <p>{blurb}</p>
+        {images.length > 0 && (
+          <Carousel variant="dark">{images.map(makeCarouselItem)}</Carousel>
+        )}
+      </Accordion.Body>
+    </Accordion.Item>
+  );
+};
+
+export { HistoryCard, type HistoryCardProps };
