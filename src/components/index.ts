d024d518cc8d6b7371e62684299efc85ea60e450
Joey Muller <jmullercuber@gmail.com>
2024-01-05 05:03:50 +0000

    fix merge

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { BasePage } from "./BasePage/BasePage";
 export { Footer } from "./Footer/footer";
+export { HistoryCard, type HistoryCardProps } from "./HistoryCard/historyCard";
 export { NavBar } from "./NavBar/navBar";
 export { SEO } from "./SEO/seo";
 export { SponsorCard } from "./SponsorCard/sponsorCard";
