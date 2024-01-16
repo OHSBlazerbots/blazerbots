a9ff8e52ebaa49155ca2de4e0632f756b6691a83
Joey Muller <jmullercuber@gmail.com>
2024-01-16 03:20:37 +0000

    footer add back "Follow Us", plus styling

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,35 +1,33 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { footer } from './footer.module.css';
+import { footer, column as columnStyle } from './footer.module.css';
 import { stopClickPropagation } from "../../utils/events";
 import { TheBlueAllianceIcon } from "../Icon";
-import { AiOutlineMail } from "@react-icons/all-files/ai/AiOutlineMail"
 import { AiFillInstagram } from "@react-icons/all-files/ai/AiFillInstagram"
 import { AiFillGithub } from "@react-icons/all-files/ai/AiFillGithub";
 
-type TDisplayIcon = () => JSX.Element;
-const LinkedContactInfo = (displayIcon: TDisplayIcon, link: string, displayText?: string) => (
+type TDisplayElement = () => JSX.Element;
+const LinkedContactInfo = (displayElement: TDisplayElement, link: string) => (
     <span>
         <a href={link} className="text-white" onClick={stopClickPropagation} >
-            {displayIcon()}
-            {displayText && ` ${displayText}`}
+            {displayElement()}
         </a>
     </span>
 )
 
-const EmailInfo = LinkedContactInfo(() => AiOutlineMail({size: "16"}), "mailto:ohsblazerbots@gmail.com", "ohsblazerbots@gmail.com")
-const InstagramInfo = LinkedContactInfo(() => AiFillInstagram({size: "48"}), "https://instagram.com/ohsblazerbots")
-const GitHubInfo = LinkedContactInfo(() => AiFillGithub({size: "48"}), "https://github.com/OHSBlazerbots")
-const TBAInfo = LinkedContactInfo(() => <TheBlueAllianceIcon style={{width: "48px"}}/>, "https://www.thebluealliance.com/team/3807")
+const EmailInfo = LinkedContactInfo(() => <>ohsblazerbots@gmail.com</>, "mailto:ohsblazerbots@gmail.com")
+const InstagramInfo = LinkedContactInfo(() => AiFillInstagram({size: "32"}), "https://instagram.com/ohsblazerbots")
+const GitHubInfo = LinkedContactInfo(() => AiFillGithub({size: "32"}), "https://github.com/OHSBlazerbots")
+const TBAInfo = LinkedContactInfo(() => <TheBlueAllianceIcon style={{width: "32px"}}/>, "https://www.thebluealliance.com/team/3807")
 
 function Footer() {
   return ( 
     <footer className={footer}>
         <Container>
-                    <div>Contact Us!</div>
-                    <div>{EmailInfo}</div>
-            <hr/>
-                    <div>{InstagramInfo} {GitHubInfo} {TBAInfo}</div>
+            <Row xs={1} sm={2}>
+                <Col className={columnStyle}>Contact Us! {EmailInfo}</Col>
+                <Col className={columnStyle}>Follow Us: {InstagramInfo} {GitHubInfo} {TBAInfo}</Col>
+            </Row>
             <hr/>
             FRC Team 3807 | Overland BlazerBots
         </Container>
