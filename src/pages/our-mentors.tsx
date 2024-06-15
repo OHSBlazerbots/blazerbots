59628aae62c60190ac4a07a45f5023e817f2c72d
Sam Goldman <6364482+samgoldman@users.noreply.github.com>
2024-06-15 17:38:47 +0000

    Add top of page paragram

--- src/pages/our-mentors.tsx
+++ src/pages/our-mentors.tsx
@@ -4,8 +4,9 @@ import { Col, Row } from "react-bootstrap";
 
 import { BasePage, SEO, MentorCard } from "../components";
 import { mentorData } from "../state/mentors/data";
+import { Card } from "react-bootstrap";
 
-const title = "Team Mentors";
+const title = "Our Mentors";
 
 const styles = {
   card: {
@@ -14,14 +15,22 @@ const styles = {
     padding: '15px',
     width: '250px'
   },
-  image: {
-    height: '200px',
-    width: '250px'
+  textCard: {
+    border: 'none',
   }
 };
 
 const page = () => (
   <BasePage pageName={title}>
+    <Row xs={1} md={3} className="g-4 justify-content-center">
+    <Card style={styles.textCard}></Card>
+    <Card style={styles.textCard}>
+      <p>Above all else, FRC strives to be a place for students to learn and grow. We have long taken pride in the BlazerBots being a "student lead" team, where the students get to deside on their priorities year to year, but that doesn't mean there isn't a place for mentors. In FRC, high schooler and adults work side-by-side, learning from each other (and yes, the adults learn just as much from the students). Mentors are professionals from variety of industries and give students the opportunity to learn from their years of experience, while still maintaining control over their destiny as a team.</p>
+      <p>Below are our mentors who volunteer their time to leave a lasting impact on the next generation of engineers, scientists and leaders.</p>
+    </Card>
+    <Card style={styles.textCard}></Card>
+      </Row>
+      <br/>
     <>
       <Row xs={1} md={3} className="g-4 justify-content-center">
         {mentorData.map((mentor, idx) => (
