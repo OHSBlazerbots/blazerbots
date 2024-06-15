610eafeee08054fd0ae876982647c848e1646da6
Sam Goldman <6364482+samgoldman@users.noreply.github.com>
2024-06-15 17:39:44 +0000

    Formatting

--- src/pages/our-mentors.tsx
+++ src/pages/our-mentors.tsx
@@ -23,14 +23,15 @@ const styles = {
 const page = () => (
   <BasePage pageName={title}>
     <Row xs={1} md={3} className="g-4 justify-content-center">
-    <Card style={styles.textCard}></Card>
-    <Card style={styles.textCard}>
-      <p>Above all else, FRC strives to be a place for students to learn and grow. We have long taken pride in the BlazerBots being a "student lead" team, where the students get to deside on their priorities year to year, but that doesn't mean there isn't a place for mentors. In FRC, high schooler and adults work side-by-side, learning from each other (and yes, the adults learn just as much from the students). Mentors are professionals from variety of industries and give students the opportunity to learn from their years of experience, while still maintaining control over their destiny as a team.</p>
-      <p>Below are our mentors who volunteer their time to leave a lasting impact on the next generation of engineers, scientists and leaders.</p>
-    </Card>
-    <Card style={styles.textCard}></Card>
-      </Row>
-      <br/>
+      <Card style={styles.textCard}></Card>
+      <Card style={styles.textCard}>
+        <p>Above all else, FRC strives to be a place for students to learn and grow. We have long taken pride in the BlazerBots being a "student lead" team, where the students get to deside on their priorities year to year, but that doesn't mean there isn't a place for mentors. In FRC, high schooler and adults work side-by-side, learning from each other (and yes, the adults learn just as much from the students). Mentors are professionals from variety of industries and give students the opportunity to learn from their years of experience, while still maintaining control over their destiny as a team.</p>
+        <p>Below are our mentors who volunteer their time to leave a lasting impact on the next generation of engineers, scientists and leaders.</p>
+      </Card>
+      <Card style={styles.textCard}></Card>
+    </Row>
+    {/* I know this probably the wrong way to accomplish this, but oh well */}
+    <br/> 
     <>
       <Row xs={1} md={3} className="g-4 justify-content-center">
         {mentorData.map((mentor, idx) => (
