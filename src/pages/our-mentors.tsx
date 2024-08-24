1815d283271097644bd9c25c0ff747e7ce2d1195
Joey Muller <jmullercuber@gmail.com>
2024-08-24 19:31:16 +0000

    simplify mentor page; adjust grid sizing

--- src/pages/our-mentors.tsx
+++ src/pages/our-mentors.tsx
@@ -4,7 +4,6 @@ import { Col, Row } from "react-bootstrap";
 
 import { BasePage, SEO, MentorCard } from "../components";
 import { mentorData } from "../state/mentors/data";
-import { Card } from "react-bootstrap";
 
 const title = "Our Mentors";
 
@@ -22,39 +21,29 @@ const styles = {
 
 const page = () => (
   <BasePage pageName={title}>
-    <Row xs={1} md={3} className="g-4 justify-content-center">
-      <Card style={styles.textCard}></Card>
-      <Card style={styles.textCard}>
-        <p>
-          Above all else, FRC strives to be a place for students to learn and
-          grow. We have long taken pride in the BlazerBots being a "student
-          lead" team, where the students get to deside on their priorities year
-          to year, but that doesn't mean there isn't a place for mentors. In
-          FRC, high schooler and adults work side-by-side, learning from each
-          other (and yes, the adults learn just as much from the students).
-          Mentors are professionals from variety of industries and give students
-          the opportunity to learn from their years of experience, while still
-          maintaining control over their destiny as a team.
-        </p>
-        <p>
-          Below are our mentors who volunteer their time to leave a lasting
-          impact on the next generation of engineers, scientists and leaders.
-        </p>
-      </Card>
-      <Card style={styles.textCard}></Card>
+    <p>
+      Above all else, FRC strives to be a place for students to learn and grow.
+      We have long taken pride in the BlazerBots being a "student lead" team,
+      where the students get to deside on their priorities year to year, but
+      that doesn't mean there isn't a place for mentors. In FRC, high schooler
+      and adults work side-by-side, learning from each other (and yes, the
+      adults learn just as much from the students). Mentors are professionals
+      from variety of industries and give students the opportunity to learn from
+      their years of experience, while still maintaining control over their
+      destiny as a team.
+    </p>
+    <p>
+      Below are our mentors who volunteer their time to leave a lasting impact
+      on the next generation of engineers, scientists and leaders.
+    </p>
+    <hr color="black" />
+    <Row xs={1} md={2} lg={3} xl={3} xxl={4} className="g-4 justify-content-center">
+      {mentorData.map((mentor, idx) => (
+        <Col key={idx}>
+          <MentorCard mentor={mentor} styles={styles} />
+        </Col>
+      ))}
     </Row>
-    {/* I know this probably the wrong way to accomplish this, but oh well */}
-    <br />
-    <>
-      <Row xs={1} md={3} className="g-4 justify-content-center">
-        {mentorData.map((mentor, idx) => (
-          <Col key={idx}>
-            <MentorCard mentor={mentor} styles={styles} />
-          </Col>
-        ))}
-      </Row>
-      <br />
-    </>
   </BasePage>
 );
 
