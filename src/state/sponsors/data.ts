6c35a8a8e08e38fbe614dd0e91f3496ef7d15416
Sam Goldman <6364482+samgoldman@users.noreply.github.com>
2026-01-04 22:16:53 -0700

    Update sponsors for 2026 season (#67)

--- src/state/sponsors/data.ts
+++ src/state/sponsors/data.ts
@@ -7,39 +7,44 @@ import logo_ace from "../../images/logo_ace.png";
 import logo_fastenal from "../../images/logo_fastenal.png";
 import logo_kokoro from "../../images/logo_kokoro.jpg";
 import logo_medtronic from "../../images/logo_medtronic.png";
-import logo_abacus from "../../images/logo_abacus.png";
-import logo_sierrafp from "../../images/logo_sierrafp.svg";
 import logo_ula from "../../images/logo_ula.png";
 import logo_onlinemetals from "../../images/logo_onlinemetals.png";
-
+import logo_merrick from "../../images/logo_merrick.png";
+import logo_bae from "../../images/logo_bae.svg";
+import logo_yea from "../../images/logo_yea.png";
+import logo_te_connectivity from "../../images/logo_te_connectivity.svg";
+import logo_campos from "../../images/logo_campos.png";
 
 //
 // Actual data for all of our sponsors
 //
 const styles = {
-  ccsf: { background: "rgb(8, 97, 136)" },
+  merrick: { background: "#002E6D" },
 };
 
 const pinkSponsors: SponsorProps[] = [
   { name: "Cherry Creek School District", logo: logo_ccsd, webpage: "https://www.cherrycreekschools.org/" },
   { name: "Comcast", logo: logo_comcast, webpage: "https://corporate.comcast.com/" },
+  { name: "Gene Haas Foundation", logo: logo_haas, webpage: "https://ghaasfoundation.org" },
+  { name: "Merrick", logo: logo_merrick, webpage: "https://www.merrick.com", style: styles.merrick },
 ];
 
 const goldSponsors: SponsorProps[] = [
-  { name: "Gene Haas Foundation", logo: logo_haas, webpage: "https://ghaasfoundation.org" },
+  { name: "ACE Hardware", logo: logo_ace, description: "Buckley Square", webpage: "https://www.acehardware.com/store-details/17489" },
+  { name: "BAE Systems", logo: logo_bae, webpage: "https://www.baesystems.com/en-us/home" },
   { name: "Medtronic", logo: logo_medtronic, webpage: "https://www.medtronic.com" },
+  { name: "TE Connectivity", logo: logo_te_connectivity, webpage: "https://www.te.com/" },
+  { name: "Young Engineers Association", logo: logo_yea, webpage: "https://www.youngengineers.us/" },
 ];
 
 const silverSponsors: SponsorProps[] = [
-  { name: "Abacus Cabinetry", logo: logo_abacus, webpage: "https://www.abacuscabinetry.com"},
-  { name: "ACE Hardware", logo: logo_ace, description: "Buckley Square", webpage: "https://www.acehardware.com/store-details/17489" },
+  { name: "Campos EPC", logo: logo_campos, webpage: "https://www.camposepc.com/"},
   { name: "Fastenal", logo: logo_fastenal, webpage: "https://www.fastenal.com" },
+  { name: "United Launch Alliance", logo: logo_ula, webpage: "https://www.ulalaunch.com/"},
 ];
 
 const bronzeSponsors: SponsorProps[] = [
   { name: "Kokoro", logo: logo_kokoro, webpage: "https://www.kokororestaurants.com/"},
-  { name: "Sierra Forest Products", logo: logo_sierrafp, webpage: "https://www.sierrafp.com/"},
-  { name: "United Launch Alliance", logo: logo_ula, webpage: "https://www.ulalaunch.com/"},
   { name: "Online Metals", logo: logo_onlinemetals, webpage: "https://www.onlinemetals.com/"},
 ];
   
